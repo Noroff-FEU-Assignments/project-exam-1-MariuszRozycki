43e61f1dbd04fb2f0a7a512412e8d996b3bb4e9b
MariuszRozycki <mariansc83@icloud.com>
2023-02-10 19:16:23 +0100

    Changed styling on page about

--- js/index.js
+++ js/index.js
@@ -21,6 +21,7 @@ async function getPageData() {
 
     for (let result of results) {
       console.log("pageHomeUrl", result.content.rendered);
+      console.log(result.content.rendered);
       renderPageHtml(result);
     }
   }
